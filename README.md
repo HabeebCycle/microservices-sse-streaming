73949110c9246d27c38d715959f9be615ff67c7d
Habeeb Okunade <guessy2k2@yahoo.com>
2021-04-19 10:38:57 +1000

    README file

---
+++ README.md
@@ -0,0 +1,8 @@
+#Microservices Using Server Sent Event and Queue Tech (RabbitMQ or Kafka)
+
+It consists of 3 microservices that call each other with a utility project that serves all the domain payloads, server and exceptions.
+
+Start Service-B then service-A for server-sent-events in 3 seconds interval
+
+To use Queue/Messaging architecture, run docker-compose for any of Kafka or RabbitMQ 
+(Note: Kafka also needs Zooker to be started) and start Service-C.
---
+++ microservices/service-b/src/main/java/com/habeebcycle/microservice/serviceb/messaging/MessagesConsumer.java
@@ -0,0 +1,41 @@
+package com.habeebcycle.microservice.serviceb.messaging;
+
+import com.habeebcycle.sse.util.exceptions.EventProcessingException;
+import com.habeebcycle.sse.util.payload.MessagePayload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+import reactor.core.publisher.FluxSink;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.Consumer;
+
+@Component
+public class MessagesConsumer implements Consumer<FluxSink<MessagePayload>> {
+
+    private final Logger LOG = LoggerFactory.getLogger(MessagesConsumer.class);
+
+    private final BlockingQueue<MessagePayload> messageQueue = new LinkedBlockingQueue<>();
+
+    @Bean
+    public Consumer<MessagePayload> messageConsumer() {
+        return messagePayload -> {
+            LOG.info("Consuming message from the Queue {}", messagePayload);
+            this.messageQueue.offer(messagePayload);
+        };
+    }
+
+    @Override
+    public void accept(FluxSink<MessagePayload> messagePayloadFluxSink) {
+        try {
+            MessagePayload messagePayload = messageQueue.take();
+            messagePayloadFluxSink.next(messagePayload);
+        } catch (InterruptedException exception) {
+            ReflectionUtils.rethrowRuntimeException(exception);
+            throw new EventProcessingException(exception);
+        }
+    }
+}
